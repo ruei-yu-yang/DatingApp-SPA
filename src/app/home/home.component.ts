7863387a91cdce540db179e4d746af70c9c595c6
Ruei Yu <vip8209@yahoo.com.tw>
2018-11-10 13:08:25 +0800

    add the register method to the auth service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,28 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  values: any;
   registerMode = true;
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
   }
 
-  getValues() {
-    this.http.get('https://localhost:44353/api/values').subscribe(
-      response => {
-        this.values = response;
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
 
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AuthService } from './../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,16 +7,22 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
-  @Output() cancelRegister  = new EventEmitter();
+  @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {}
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(
+      () => {
+        console.log('registeration success!');
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
   cancel() {

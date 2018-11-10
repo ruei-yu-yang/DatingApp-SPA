d66969a70cf3e8e20b99113140939cc5c4d9264c
Ruei Yu <vip8209@yahoo.com.tw>
2018-11-10 12:54:01 +0800

    parent to child component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { ValueComponent } from './value/value.component';
-import { AuthService } from './_services/auth.service';
-import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
       NavComponent,
       RegisterComponent,
       HomeComponent
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,12 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  values: any;
   registerMode = false;
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getValues();
+  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getValues() {
+    this.http.get('https://localhost:44353/api/values').subscribe(
+      response => {
+        this.values = response;
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 }

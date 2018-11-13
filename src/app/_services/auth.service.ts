7e51ac3052f2115912bfc8907e8544bcc4612e2b
Ruei Yu <vip8209@yahoo.com.tw>
2018-11-13 11:49:36 +0800

    using the anuglar jwt library to decode tokens

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -8,6 +8,7 @@ import { map } from 'rxjs/operators';
 export class AuthService {
   baseUrl = 'https://localhost:44353/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) {}
 
@@ -17,6 +18,8 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+          console.log(this.decodedToken);
         }
       })
     );

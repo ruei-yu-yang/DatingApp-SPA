a23b311eba7d30d6b60ac91e50f8c548d703b83e
Ruei Yu <vip8209@yahoo.com.tw>
2018-11-20 17:28:18 +0800

    finishing off the member edit component

--- src/app/members/member-edit/member-edit.component.ts
+++ src/app/members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_model/user';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from './../../_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -12,7 +14,9 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 export class MemberEditComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private userService: UserService,
+    private authService: AuthService
   ) {}
   @ViewChild('editForm') editForm: NgForm;
   user: User;
@@ -30,8 +34,10 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
+this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(
+  next => {
     this.alertify.success('Profile updated successfully');
     this.editForm.reset(this.user);
+  }, error => { this.alertify.error(error); });
   }
 }

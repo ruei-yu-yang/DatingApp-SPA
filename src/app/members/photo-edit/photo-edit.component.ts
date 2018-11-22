47a9c68a3c94d959fc3384de7a57df95f6d88265
Ruei Yu <vip8209@yahoo.com.tw>
2018-11-22 15:44:26 +0800

    adding the delete photo functionality

--- src/app/members/photo-edit/photo-edit.component.ts
+++ src/app/members/photo-edit/photo-edit.component.ts
@@ -85,4 +85,20 @@ export class PhotoEditComponent implements OnInit {
         err => this.alertify.error(err)
       );
   }
+
+  deletePhoto(id: number) {
+    this.alertify.confirm('Are you sure you want to delete this photo?', () => {
+      this.userService
+        .deletePhoto(this.authService.decodedToken.nameid, id)
+        .subscribe(
+          () => {
+            this.photos.splice(this.photos.findIndex(p => p.id === id), 1);
+            this.alertify.success('Photo has been deleted');
+          },
+          err => {
+            this.alertify.error('Failed to delete the photo');
+          }
+        );
+    });
+  }
 }

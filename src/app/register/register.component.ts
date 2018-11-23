a9139bc6a734b7083e0e346499fffbad86c0e1c9
Ruei Yu <vip8209@yahoo.com.tw>
2018-11-23 10:04:02 +0800

    validation in reactive forms

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertifyService } from './../_services/alertify.service';
 import { AuthService } from './../_services/auth.service';
 
@@ -20,9 +20,13 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      username: new FormControl(),
-      password: new FormControl(),
-      confirmPassword: new FormControl()
+      username: new FormControl('Hello', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(8),
+        Validators.minLength(4)
+      ]),
+      confirmPassword: new FormControl('', [Validators.required])
     });
   }
 

f6605a397f333e60938f63e767a945c550c50555
Ruei Yu <vip8209@yahoo.com.tw>
2018-11-24 10:41:23 +0800

    completing the registration page

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_model/user';
 import { AlertifyService } from './../_services/alertify.service';
 import { AuthService } from './../_services/auth.service';
 
@@ -11,19 +13,20 @@ import { AuthService } from './../_services/auth.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
   constructor(
     private authService: AuthService,
     private alertify: AlertifyService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private router: Router
   ) {}
 
   ngOnInit() {
     this.bsConfig = {
-      containerClass : 'theme-red'
+      containerClass: 'theme-red'
     };
     this.createRegisterForm();
   }
@@ -60,15 +63,22 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    // this.authService.register(this.model).subscribe(
-    //   () => {
-    //     this.alertify.success('registeration success!');
-    //   },
-    //   error => {
-    //     this.alertify.error(error);
-    //   }
-    // );
-    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(
+        () => {
+          this.alertify.success('Registeration successful');
+        },
+        error => {
+          this.alertify.error(error);
+        },
+        () => {
+          this.authService.login(this.user).subscribe(() => {
+            this.router.navigate(['/members']);
+          });
+        }
+      );
+    }
   }
 
   cancel() {
